2180b7ddd2a6307372cf4ad1b37931866c3c7a22
ejohnguia <35359631+ejohnguia@users.noreply.github.com>
2023-02-01 15:22:11 -0700

    add: copyrights prop

---
+++ src/view/assets/props/Copyrights.js
@@ -0,0 +1,32 @@
+/** @format */
+
+import Typography from "@mui/material/Typography";
+import Link from "@mui/material/Link";
+
+export default function Copyright(props) {
+	return (
+		<Typography
+			variant="body2"
+			color="text.secondary"
+			align="center"
+			fontSize="11px"
+			{...props}
+		>
+			{"By using this service, you agree to our "}
+			<Link
+				color="inherit"
+				href="https://brightsquid.com/pages/brightsquid-application-terms-of-use"
+			>
+				Terms of Use
+			</Link>{" "}
+			{" and "}
+			<Link
+				color="inherit"
+				href="https://brightsquid.com/pages/brightsquid-application-privacy-policy"
+			>
+				Privacy Policy
+			</Link>
+			{""}
+		</Typography>
+	);
+}

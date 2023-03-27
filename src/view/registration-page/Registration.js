07f89ee2f6035d442b5907fa80c76f111285fa94
kg-sicat <kylie.sicat@gmail.com>
2023-03-26 23:34:05 -0600

    password meter works!

--- src/view/registration-page/Registration.js
+++ src/view/registration-page/Registration.js
@@ -1,6 +1,7 @@
 /** @format */
 // import brightsquidLogo from '../../img/brightsquid_logo.PNG';
 import * as React from "react";
+import App from "../../App.js";
 import Avatar from "@mui/material/Avatar";
 import Button from "@mui/material/Button";
 import CssBaseline from "@mui/material/CssBaseline";
@@ -14,9 +15,7 @@ import Grid from "@mui/material/Grid";
 import LockOutlinedIcon from "@mui/icons-material/LockOutlined";
 import Typography from "@mui/material/Typography";
 import { createTheme, ThemeProvider } from "@mui/material/styles";
-import PasswordStrengthBar from 'react-password-strength-bar';
 import Copyright from "../assets/props/Copyrights.js";
-
 import MedicalPhoto from "../assets/props/MedicalPhotoProp.js";
 
 // For Step count
@@ -36,15 +35,34 @@ import InputAdornment from '@mui/material/InputAdornment';
 import Visibility from '@mui/icons-material/Visibility';
 import VisibilityOff from '@mui/icons-material/VisibilityOff';
 import OutlinedInput from '@mui/material/OutlinedInput';
+import { stepperClasses } from "@mui/material";
+
+// PAssword Meter stuff
+import { passwordStrength } from 'check-password-strength';
+import zxcvbn from 'zxcvbn';
+import PasswordStrengthBar from 'react-password-strength-bar';
+
+// https://www.npmjs.com/package/check-password-strength
+// https://www.npmjs.com/package/react-password-strength-bar
+
+// NOT USED
+// // Testing Password from youtube vid
+// import {Password} from "primereact/password";
+// import "primereact/resources/themes/lara-light-indigo/theme.css"; // theme
+// import "primereact/resources/primereact.min.css"; // core css
+// import "primeicons/primeicons.css"; //icons
+
+// function PasswordMeterComponent() {
+// 	const [inputPassword, setInputValue] = useState('');
+// 	password = inputPassword
+// 	return <PasswordMeter password/>;
+// }
 
 // import HorizontalLinearStepper from "../../stepper";
 // import setActiveStep from "../../stepper";
 
-import { stepperClasses } from "@mui/material";
-
 const theme = createTheme();
-// const { password } = this.state;
-// <PasswordStrengthBar password={password} />
+
 export default function Registration() {
 	const [showPassword, setShowPassword] = React.useState(false);
 	const handleClickShowPassword = () => setShowPassword((show) => !show);
@@ -53,6 +71,46 @@ export default function Registration() {
 	  event.preventDefault();
 	};
 
+	// PASSWORD METER FUNCTIONS AND VARIABLES
+	// Import the state from passwordStrength npm
+	const { passwordStrength } = require('check-password-strength');
+
+	// Create a state var to hold the password strength
+	// const [passwordStrength, setPasswordStrength] = React.useState(0);
+	const [password, setPassword] = React.useState("");
+
+	// Usage for the react-password-strength bar
+	// const { password } = this.state;
+
+	const handlePasswordChange = (event) => {
+		const { value } = event.target;
+		const strength = zxcvbn(value).score;
+		setPassword(value);
+		console.log(password);
+	  };
+
+	function GetPasswordValue() {
+		const [inputValue, setInputValue] = React.useState('');
+		
+		const handlePasswordChange = (event) => {
+			const { value } = event.target;
+			const strength = zxcvbn(value).score;
+			setPassword(strength);
+		  };
+		
+		return (
+			<div>
+			<OutlinedInput
+				label="My Input"
+				value={inputValue}
+				onChange={handlePasswordChange}
+			/>
+			<p>The input value is: {inputValue}</p>
+			</div>
+		);
+	}
+	  
+	
 	return (
 		<ThemeProvider theme={theme}>
 			<Grid container component="main" sx={{ height: "100%" , width: "100%"}} marginLeft={4}>
@@ -64,7 +122,6 @@ export default function Registration() {
 					sm={4}
 					md={3}
 					sx={{
-						// width: "33%",
 						backgroundImage: MedicalPhoto,
 						backgroundRepeat: "no-repeat",
 						backgroundColor: (t) =>
@@ -99,14 +156,14 @@ export default function Registration() {
 						<Grid item xs={4} mt={4}>
 						<img src={""} width={141.8} height={30.8} alt="brightsquid"/>
 						</Grid>
-						<Typography component="h1" variant="h5">
+						<Typography component="body" variant="h5">
 							Welcome to Brightsquid!
 						</Typography>
 						<Box
 							component="form"
 							sx={{ mt: 1 }}
 						>
-							<Typography variant="body1"> Username </Typography>
+							<Typography variant="bodyBS"> Username </Typography>
 							<TextField
 								margin="normal"
 								required
@@ -153,6 +210,8 @@ export default function Registration() {
 								fullWidth
 								id="outlined-adornment-password"
 								type={showPassword ? 'text' : 'password'}
+								// value={inputValue}
+								onChange={handlePasswordChange}
 								endAdornment={
 								<InputAdornment position="end">
 									<IconButton
@@ -164,11 +223,32 @@ export default function Registration() {
 									{showPassword ? <VisibilityOff /> : <Visibility />}
 									</IconButton>
 								</InputAdornment>
+								
 								}
 							/>
 							{/* TODO: Password meter */}
 							<Typography variant="body1"> PASSWORD METER PLACEHOLDER </Typography>
+
+							<PasswordStrengthBar password={password} />
+							
+
 							<Typography variant="body1"> Confirm Password </Typography>
+							{/* <Password value={value} onChange={(e) => setValue(e.target.value)} feedback={false} /> */}
+							
+							{/* <PasswordStrengthBar password={password} /> */}
+							{/* function PasswordMeterK() {
+								return (
+									<div className="PasswordMeterK">
+										<div style={{padding: "40px"}}>
+										<Password feedback={false}
+
+											/>
+										</div>
+									</div>
+											
+								);
+
+							} */}
 
 							<OutlinedInput
 								label="Password"
@@ -188,6 +268,43 @@ export default function Registration() {
 								</InputAdornment>
 								}
 							/>
+
+							<OutlinedInput
+								label="Password"
+								fullWidth
+								id="outlined-adornment-password"
+								type={showPassword ? 'text' : 'password'}
+								endAdornment={
+								<InputAdornment position="end">
+									<IconButton
+									aria-label="toggle password visibility"
+									onClick={handleClickShowPassword}
+									onMouseDown={handleMouseDownPassword}
+									edge="end"
+									>
+									{showPassword ? <VisibilityOff /> : <Visibility />}
+									</IconButton>
+								</InputAdornment>
+								}
+							/>
+							{/* About you section */}
+							<Typography variant="h6">
+										About You 
+									</Typography>
+
+							<Grid item xs={5} mt={5}>
+									<Typography  mb={2}>Title <strong className='asterisk' style={{color: "red"}}> * </strong></Typography>
+									<FormControl fullWidth>
+									<InputLabel> Title </InputLabel>
+									{/* TO-DO: fill with actual data */}
+									<Select id="clinic-select" label="Clinic Type">
+										<MenuItem value={0}>Dental</MenuItem>
+										<MenuItem value={0}>Family</MenuItem>
+										<MenuItem value={0}>Hospital</MenuItem>
+										<MenuItem value={0}>Mental Health </MenuItem>
+									</Select>
+									</FormControl>
+									</Grid>
 							<Button
 								id="next-steps-btn"
 								component={Link}
@@ -208,7 +325,10 @@ export default function Registration() {
 								<HorizontalLinearStepper> </HorizontalLinearStepper>
 								<Grid item>
 								</Grid>
+							
 							</Grid>
+
+							
 						</Box>
 					</Box>
 					<Copyright sx={{ mt: 5 }} />

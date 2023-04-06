7f82e1af0f3c321226e4fd2f5397b7c381003b06
ejohnguia <35359631+ejohnguia@users.noreply.github.com>
2023-04-05 20:31:19 -0600

    add: post clinic and get clinics

--- server/routes.js
+++ server/routes.js
@@ -2,7 +2,12 @@
 
 const express = require("express");
 const userModel = require("./schemas/Users.model");
+const clinicModel = require("./schemas/Clinic.model");
 const app = express();
+
+// =================================
+// ---------- USER ROUTES ----------
+// =================================
 
 // Adds new user into the db
 app.post("/add_user", async (request, response) => {
@@ -22,6 +27,33 @@ app.get("/users", async (request, response) => {
 
 	try {
 		response.send(users);
+	} catch (error) {
+		response.status(500).send(error);
+	}
+});
+
+// =================================
+// --------- CLINIC ROUTES ---------
+// =================================
+
+// Adds new clinic into the db
+app.post("/add_clinic", async (request, response) => {
+	const clinic = new clinicModel(request.body);
+
+	try {
+		await clinic.save();
+		response.send(clinic);
+	} catch (error) {
+		response.status(500).send(error);
+	}
+});
+
+// Gets all clinics
+app.get("/clinics", async (request, response) => {
+	const clinics = await clinicModel.find({});
+
+	try {
+		response.send(clinics);
 	} catch (error) {
 		response.status(500).send(error);
 	}

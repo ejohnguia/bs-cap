e03b4abb312443525455302bf9379d54003d5d77
gab <pili.gabrielle@yahoo.ca>
2023-04-10 00:12:57 -0600

    Test Clean up and comments

--- tests/registration_tests.py
+++ tests/registration_tests.py
@@ -5,63 +5,70 @@ from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.action_chains import ActionChains
-from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
 import time
 import re
 
-class RegistrationTests(unittest.TestCase):
+main_link = "http://127.0.0.1:3000"
+registration_link = "http://127.0.0.1:3000/registration"
+practice_details_link = "http://127.0.0.1:3000/registration/practice-details"
+privacy_link = "https://brightsquid.com/pages/brightsquid-application-privacy-policy"
+terms_link = "https://brightsquid.com/pages/brightsquid-application-terms-of-use"
+final_link = "http://127.0.0.1:3000/final"
 
+'''
+    This class contains unit tests for the 'Welcome' Page
+    - 3 Unit Tests
+    - Currently have 3/3 Passing 
+'''
+class WelcomePageTests(unittest.TestCase):
     def setUp(self):
         self.driver = webdriver.Chrome() # use firefox or change to chrome
 
-        # links 
-        self.main_link = "http://127.0.0.1:3000"
-        self.registration_link = "http://127.0.0.1:3000/registration"
-        self.practice_details_link = "http://127.0.0.1:3000/registration/practice-details"
-        self.privacy_link = "https://brightsquid.com/pages/brightsquid-application-privacy-policy"
-        self.terms_link = "https://brightsquid.com/pages/brightsquid-application-terms-of-use"
-        self.final_link = "http://127.0.0.1:3000/final"
-
-
-    # ########################################### WELCOME PAGE UNIT TESTS ############################################
-    # # TODO: test for information message (REQ-005)
-    # # def testWelcomeInfoMssg(self):
-    
-
     # test for privacy link (REQ-003)
     def testWelcomePrivacyLink(self):
-        self.driver.get(self.main_link)
+        self.driver.get(main_link)
 
         react_app = WebDriverWait(self.driver, 5).until(EC.presence_of_element_located((By.ID, "root")))
 
-        react_app.find_element(By.ID, "privacy-policy").click()     # click privacy policy 
-        self.assertTrue(self.privacy_link, self.driver.current_url) # check if the url matches 
-        self.driver.back()                                          # go back to previous page
+        react_app.find_element(By.ID, "privacy-policy").click() # click privacy policy 
+        self.assertTrue(privacy_link, self.driver.current_url)  # check if the url matches 
+        self.driver.back()                                      # go back to previous page
 
     # test for terms of use link (REQ-004)
     def testWelcomeTermsLink(self):
-        self.driver.get(self.main_link)
+        self.driver.get(main_link)
 
         react_app = WebDriverWait(self.driver, 5).until(EC.presence_of_element_located((By.ID, "root")))
         
-        react_app.find_element(By.ID, "terms-of-use").click()       # click privacy policy 
-        self.assertTrue(self.terms_link, self.driver.current_url)   # check if the url matches 
-        self.driver.back()                                          # go back to previous page
+        react_app.find_element(By.ID, "terms-of-use").click()  # click privacy policy 
+        self.assertTrue(terms_link, self.driver.current_url)   # check if the url matches 
+        self.driver.back()                                     # go back to previous page
 
     # test next button (REQ-001)
     def testWelcomeNextButton(self):
-        self.driver.get(self.main_link)
+        self.driver.get(main_link)
 
         react_app = WebDriverWait(self.driver, 5).until(EC.presence_of_element_located((By.ID, "root")))
 
         react_app.find_element(By.ID, "setup-btn").click()                 # clicks on "Next Steps" button 
-        self.assertTrue(self.registration_link, self.driver.current_url)        # check if you're in the registration page 
+        self.assertTrue(registration_link, self.driver.current_url)        # check if you're in the registration page 
     
-    # ########################################### REGISTRATION PAGE UNIT TESTS ############################################
-    # TODO: testing for password meter criteria (REQ-006 - REQ-012)
+    def tearDown(self):
+        print("Test completed: ", self.id())
+        self.driver.quit() 
+
+'''
+    This class contains unit tests for the 'Registration' Page
+    - 12 Unit Tests
+    - Currently have 9/12 Passing 
+'''
+class RegistrationPageTests(unittest.TestCase):
+    def setUp(self):
+        self.driver = webdriver.Chrome() # use firefox or change to chrome
+
     # User shouldn't be able to create an account with just lowercase letters and meeting minimum 11 characters
     def testRegistrationPasswordMeterCriteriaLowercaseOnly(self):
-        self.driver.get(self.registration_link)
+        self.driver.get(registration_link)
 
         react_app = WebDriverWait(self.driver, 5).until(EC.presence_of_element_located((By.ID, "root")))
         
@@ -78,7 +85,7 @@ class RegistrationTests(unittest.TestCase):
 
     # See if user is able to create an account with just uppercase letters and meeting minimum 11 characters
     def testRegistrationPasswordMeterCriteriaUppercaseOnly(self):
-        self.driver.get(self.registration_link)
+        self.driver.get(registration_link)
 
         react_app = WebDriverWait(self.driver, 5).until(EC.presence_of_element_located((By.ID, "root")))
         
@@ -95,7 +102,7 @@ class RegistrationTests(unittest.TestCase):
 
     # User shouldn't be able to create an account with just numbers
     def testRegistrationPasswordMeterCriteriaNumbersOnly(self):
-        self.driver.get(self.registration_link)
+        self.driver.get(registration_link)
 
         react_app = WebDriverWait(self.driver, 5).until(EC.presence_of_element_located((By.ID, "root")))
         
@@ -112,7 +119,7 @@ class RegistrationTests(unittest.TestCase):
 
     # User shouldn't be able to create an account with just special chars
     def testRegistrationPasswordMeterCriteriaSpecialCharOnly(self):
-        self.driver.get(self.registration_link)
+        self.driver.get(registration_link)
 
         react_app = WebDriverWait(self.driver, 5).until(EC.presence_of_element_located((By.ID, "root")))
 
@@ -129,7 +136,7 @@ class RegistrationTests(unittest.TestCase):
 
     # Same characters i should fail
     def testRegistrationPasswordWeakSameChar(self):
-        self.driver.get(self.registration_link)
+        self.driver.get(registration_link)
 
         react_app = WebDriverWait(self.driver, 5).until(EC.presence_of_element_located((By.ID, "root")))
 
@@ -145,7 +152,7 @@ class RegistrationTests(unittest.TestCase):
         self.assertNotEqual("User created successfully", alert.text)   
 
     def testRegistrationPasswordWeakLessThan11(self):
-        self.driver.get(self.registration_link)
+        self.driver.get(registration_link)
 
         react_app = WebDriverWait(self.driver, 5).until(EC.presence_of_element_located((By.ID, "root")))
 
@@ -162,26 +169,26 @@ class RegistrationTests(unittest.TestCase):
 
     # test for privacy link (REQ-003)
     def testRegistrationPrivacyLink(self):
-        self.driver.get(self.registration_link)
+        self.driver.get(registration_link)
 
         react_app = WebDriverWait(self.driver, 5).until(EC.presence_of_element_located((By.ID, "root")))
 
         react_app.find_element(By.ID, "privacy-policy").click()     # click privacy policy 
-        self.assertTrue(self.privacy_link, self.driver.current_url) # check if the url matches 
+        self.assertTrue(privacy_link, self.driver.current_url) # check if the url matches 
         self.driver.back()                                          # go back to previous page
 
     # test for terms of use link (REQ-004)
     def testRegistrationTermsLink(self):
-        self.driver.get(self.registration_link)
+        self.driver.get(registration_link)
 
         react_app = WebDriverWait(self.driver, 5).until(EC.presence_of_element_located((By.ID, "root")))
         
         react_app.find_element(By.ID, "terms-of-use").click()       # click privacy policy 
-        self.assertTrue(self.terms_link, self.driver.current_url)   # check if the url matches 
+        self.assertTrue(terms_link, self.driver.current_url)   # check if the url matches 
         self.driver.back()           
 
     def testRegistrationPasswordToggle(self):
-        self.driver.get(self.registration_link)
+        self.driver.get(registration_link)
         react_app = WebDriverWait(self.driver, 5).until(EC.presence_of_element_located((By.ID, "root")))
 
         pass_type = react_app.find_element(By.ID, "password").get_attribute("type") # get type of textfield
@@ -190,7 +197,7 @@ class RegistrationTests(unittest.TestCase):
 
     # test if passwords match (REQ-013) 
     def testRegistrationConfirmPassError(self):
-        self.driver.get(self.registration_link)
+        self.driver.get(registration_link)
         react_app = WebDriverWait(self.driver, 5).until(EC.presence_of_element_located((By.ID, "root")))
 
         pass_field = react_app.find_element(By.ID, "password")
@@ -207,7 +214,7 @@ class RegistrationTests(unittest.TestCase):
 
     # test if password is entered (REQ-014) 
     def testRegistrationEmptyPassError(self):
-        self.driver.get(self.registration_link)
+        self.driver.get(registration_link)
         react_app = WebDriverWait(self.driver, 5).until(EC.presence_of_element_located((By.ID, "root")))
 
         # fill in username field
@@ -222,7 +229,7 @@ class RegistrationTests(unittest.TestCase):
 
     # test next button (REQ-001)    
     def testRegistrationNextStepsButton(self):
-        self.driver.get(self.registration_link)
+        self.driver.get(registration_link)
         react_app = WebDriverWait(self.driver, 5).until(EC.presence_of_element_located((By.ID, "root")))
         
         user_field = react_app.find_element(By.ID, "username")
@@ -239,46 +246,58 @@ class RegistrationTests(unittest.TestCase):
         alert = self.driver.switch_to.alert                                     # switch to alert window
         alert.accept()                                                          # accept the alert to close
 
-        self.assertTrue(self.practice_details_link, self.driver.current_url)    # check if it brings you to the Practice Details page
+        self.assertTrue(practice_details_link, self.driver.current_url)    # check if it brings you to the Practice Details page
 
-    ########################################### PRACTICE DETAILS PAGE UNIT TESTS ############################################
+    def tearDown(self):
+        print("Test completed: ", self.id())
+        self.driver.quit() 
+
+'''
+    This class contains unit tests for the 'Practice Details' Page
+    - 7 Unit Tests
+    - Currently have 6/7 Passing 
+'''
+class PracticeDetailsPageTests(unittest.TestCase):
+    def setUp(self):
+        self.driver = webdriver.Chrome() # use firefox or change to chrome
+
     # test for privacy link (REQ-003)
     def testPracticeDetailsPagePrivacyLink(self): 
-        self.driver.get(self.practice_details_link)
+        self.driver.get(practice_details_link)
 
         react_app = WebDriverWait(self.driver, 5).until(EC.presence_of_element_located((By.ID, "root")))
 
-        react_app.find_element(By.ID, "privacy-policy").click()     # click privacy policy 
-        self.assertTrue(self.privacy_link, self.driver.current_url) # check if the url matches 
+        react_app.find_element(By.ID, "privacy-policy").click() # click privacy policy 
+        self.assertTrue(privacy_link, self.driver.current_url)  # check if the url matches 
         self.driver.back()       
 
     # test for terms link (REQ-004)
     def testPracticeDetailsPageTermsLink(self): 
-        self.driver.get(self.practice_details_link)
+        self.driver.get(practice_details_link)
 
         react_app = WebDriverWait(self.driver, 5).until(EC.presence_of_element_located((By.ID, "root")))
         
         react_app.find_element(By.ID, "terms-of-use").click()       # click privacy policy 
-        self.assertTrue(self.terms_link, self.driver.current_url)   # check if the url matches 
+        self.assertTrue(terms_link, self.driver.current_url)   # check if the url matches 
         self.driver.back()                                          # go back to previous page
 
     # test if the next button won't work with empty required fields (REQ-015)
     def testPracticeDetailsPageNextButtonEmptyFields(self): 
-        self.driver.get(self.practice_details_link)
+        self.driver.get(practice_details_link)
         react_app = WebDriverWait(self.driver, 5).until(EC.presence_of_element_located((By.ID, "root")))
         time.sleep(5)
         attr = react_app.find_element(By.ID, "next-btn").get_attribute("aria-disabled") # check if button is disabled
-        self.assertEqual(attr, "true")                               
+        self.assertEqual("true", attr)                               
 
     # test next button after all required fields are filled (REQ-001)
     def testPracticeDetailsPageNextButtonRequiredFields(self): 
-        self.driver.get(self.practice_details_link)
+        self.driver.get(practice_details_link)
         react_app = WebDriverWait(self.driver, 5).until(EC.presence_of_element_located((By.ID, "root")))
         keyDown = ActionChains(react_app)
 
         # obtain all required fields
         practice_name = react_app.find_element(By.ID, "practice_name")          # get practice name field
-        clinic_type = react_app.find_element(By.ID, "orgType-select")            # get clinic type field
+        clinic_type = react_app.find_element(By.ID, "orgType-select")           # get clinic type field
         clinic_subtype = react_app.find_element(By.ID, "orgSubType-select")     # get subtype field
         province = react_app.find_element(By.ID, "province-select")             # get province field
 
@@ -302,20 +321,20 @@ class RegistrationTests(unittest.TestCase):
         prov_menu.find_element(By.ID, "Alberta").send_keys(Keys.RETURN)
         time.sleep(2) 
 
-        react_app.find_element(By.ID, "next-btn").click()            # click next button
-        self.assertTrue(self.final_link, self.driver.current_url)    # check if we are in the final page     
+        react_app.find_element(By.ID, "next-btn").click()       # click next button
+        self.assertTrue(final_link, self.driver.current_url)    # check if we are in the final page     
     
     # test if back button brings us to registration page (REQ-016)
     def testPracticeDetailsPageBackButton(self):
-        self.driver.get(self.practice_details_link)
+        self.driver.get(practice_details_link)
         react_app = WebDriverWait(self.driver, 5).until(EC.presence_of_element_located((By.ID, "root")))
     
-        react_app.find_element(By.ID, "back-btn").click()                  # click back button
-        self.assertTrue(self.registration_link, self.driver.current_url)   # check if the url matches the previous page          
+        react_app.find_element(By.ID, "back-btn").click()             # click back button
+        self.assertTrue(registration_link, self.driver.current_url)   # check if the url matches the previous page          
 
     # test if the field only contains numbers - (REQ-017)
     def testPracticeDetailsPagePhoneNumber(self):
-        self.driver.get(self.practice_details_link)
+        self.driver.get(practice_details_link)
         react_app = WebDriverWait(self.driver, 5).until(EC.presence_of_element_located((By.ID, "root")))
     
         # get practice phone field
@@ -327,8 +346,24 @@ class RegistrationTests(unittest.TestCase):
         phone_val = (phone.get_attribute("value"))               # get the value from the text field
         clean_number = re.sub(r"[\(\)\+\-]", "", phone_val)      # remove the format of the phone number with regex 
         clean_number = clean_number.replace(" ", "")             # remove any spaces
-        self.assertEqual(clean_number.isdigit(), True)          # check after all format is removed, that 
+        self.assertEqual(clean_number.isdigit(), True)           # check after all format is removed, that 
                                                                  # they're only numbers 
+    
+    # Check if data still stays on form when refreshed
+    def testPracticeDetailsPageDataRetention(self):
+        self.driver.get(practice_details_link)
+        react_app = WebDriverWait(self.driver, 5).until(EC.presence_of_element_located((By.ID, "root")))
+        
+        # get practice name field and send a value 
+        react_app.find_element(By.ID, "practice_name").send_keys("Test Practice Name")
+        
+        # refresh page
+        self.driver.refresh()
+
+        react_app = WebDriverWait(self.driver, 5).until(EC.presence_of_element_located((By.ID, "root")))
+        pratice_details_val = react_app.find_element(By.ID, "practice_name").get_attribute("value")
+
+        self.assertEqual("Test Practice Name", pratice_details_val)
 
     def tearDown(self):
         print("Test completed: ", self.id())
